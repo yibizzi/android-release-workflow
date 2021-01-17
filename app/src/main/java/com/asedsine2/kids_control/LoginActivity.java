17867b895e68255d643845fc64f59de8108a4b7c
yibizzi <ibizzi.younes@ine.inpt.ma>
2021-01-17 21:02:17 +0100

    added workflows

---
+++ app/src/main/java/com/asedsine2/kids_control/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.asedsine2.kids_control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String email, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        
+    }
+}
